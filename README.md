b03c38e1c8a067f0e54938df885073668de5bf15
Pål Grønås Drange <Pal.Drange@uib.no>
2026-08-11 09:27:13 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# pq
+
+Can you implement a priority queue without mistakes on the first attempt?
---
+++ pq.py
@@ -0,0 +1,52 @@
+pq = lambda: [None]
+left = lambda i: i * 2
+right = lambda i: i * 2 + 1
+parent = lambda i: i // 2
+leaf = lambda q, i: right(i) >= len(q)
+
+
+def _bubble_up(q, i):
+    while parent(i) and q[parent(i)] > q[i]:
+        q[parent(i)], q[i] = q[i], q[parent(i)]
+        i = parent(i)
+
+
+def _minchild(q, i):
+    l = left(i)
+    r = right(i)
+    return l if q[l] < q[r] else r
+
+
+def _bubble_down(q, i):
+    while not leaf(q, i) and q[_minchild(q, i)] < q[i]:
+        c = _minchild(q, i)
+        q[c], q[i] = q[i], q[c]
+        i = c
+
+
+def insert(q, e):
+    q.append(e)
+    _bubble_up(q, len(q) - 1)
+
+
+def pop(q):
+    e = q[1]
+    q[1] = q[len(q) - 1]
+    del q[len(q) - 1]
+    _bubble_down(q, 1)
+    return q, e
+
+
+if __name__ == "__main__":
+    from random import randint as R
+
+    N = 1000
+    Q = pq()
+    for _ in range(N):
+        insert(Q, R(N * 10, N * 100 - 1))
+
+    prev = 0
+    for _ in range(N):
+        Q, e = pop(Q)
+        assert e >= prev, f"last pop = {prev}, this pop = {e}"
+        prev = e
